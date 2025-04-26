f66b1554281a31302e7c9c6629c22a238d94708f
Lucasjr18 <lucasferraz72626@gmail.com>
2025-04-26 11:22:38 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	
+	char nome ='l';
+	int idade = 15;
+	boolean cafe = true;
+ 	int laranja = 25;
+ 	int uva = 33;
+ 	int multi  = uva * laranja;
+ 	System.out.println("resultado = " + multi);
+	
+	Scanner entrada = new Scanner(System.in);
+	System.out.print("digite o valor da uva ");	
+	 uva = entrada.nextInt();
+	System.out.print("digite o valor da laranja");
+	 laranja = entrada.nextInt();    
+	    
+	 System.out.print("resultado da soma = "+ (laranja + uva) );  
 	}
 }
