ac3bcfe9dc88ce1626f35a72bc1eac56ffc10870
Lucasjr18 <lucasferraz72626@gmail.com>
2025-05-17 11:33:03 -0300

    aula 2.java

--- aula-2/Main.java
+++ aula-2/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		Scanner entrada = new Scanner (System.in); 
+	 double coxinha = 4.50;
+	 double pastel = 12.00;
+	 double batata_frita = 23.99;
+	 System.out.println("o que vc quer pedir do menu?");
+	 String pedido = entrada.next();
+	 System.out.println("quantas unidades");
+	  String unidades = entrada.next();  
+	    
+	    
 	}
-}
